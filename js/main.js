fd8f1849918fd612fee98bd621886c6092e91e89
Luis Mendoza <luis@mendoza.io>
2014-11-27 14:19:39 -0600

    Added navigation and scrolling transitions.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(window).scroll(function() {
+    if ($("#main-nav").offset().top > 50) {
+        $("#main-nav").addClass("black");
+        $("#main-nav").removeClass("white");
+        $("#logo-black").css("display", "block");
+        $("#logo-white").css("display", "none");
+    } else {
+        $("#main-nav").addClass("white");
+        $("#main-nav").removeClass("black");
+        $("#logo-black").css("display", "none");
+        $("#logo-white").css("display", "block");
+    }
+});
