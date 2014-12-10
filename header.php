be3812630443655c4eb71933722d713821633d1e
Luis Mendoza <luis@mendoza.io>
2014-12-10 13:14:53 -0600

    Moved google analytics code.

--- header.php
+++ header.php
@@ -15,6 +15,17 @@
         <link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,300,700' rel='stylesheet' type='text/css'>
         <link href='http://fonts.googleapis.com/css?family=Roboto:500,100,300,700,400' rel='stylesheet' type='text/css'>
 
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//http://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-57462403-1', 'auto');
+          ga('send', 'pageview');
+
+        </script>
+
         <?php wp_head();?>
 
     </head>
